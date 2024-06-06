19a43fd0bd29fcc66df6e3445a9489996bb9316d
jovinkendrico <jovinkendrico@gmail.com>
2024-06-06 17:21:42 +0700

    add logo future farmers

--- app/src/main/java/com/example/futurefarmers/ui/level/LevelViewModel.kt
+++ app/src/main/java/com/example/futurefarmers/ui/level/LevelViewModel.kt
@@ -4,9 +4,30 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.futurefarmers.data.repository.MainRepository
+import com.example.futurefarmers.data.response.GetRelayConfigResponse
+import com.example.futurefarmers.data.response.UpdateRelayConfigResponse
+import com.google.gson.JsonObject
 
 class LevelViewModel(private var repository: MainRepository): ViewModel() {
+    private lateinit var levelResponse: LiveData<GetRelayConfigResponse>
+    private lateinit var updateLevelResponse: LiveData<UpdateRelayConfigResponse>
+
     fun getSession(): LiveData<String> {
         return repository.getSession().asLiveData()
+    }
+
+    fun getlecv(token: String){
+        levelResponse = repository.getRelayConfig(token)
+    }
+
+    fun getLevelResponse(): LiveData<GetRelayConfigResponse> {
+        return levelResponse
+    }
+
+    fun updateLevel(token: String, jsonObject: JsonObject){
+        updateLevelResponse = repository.updateRelayConfig(token, jsonObject)
+    }
+    fun getUpdateLevelResponse(): LiveData<UpdateRelayConfigResponse> {
+        return updateLevelResponse
     }
 }
